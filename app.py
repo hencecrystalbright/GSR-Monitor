d58d1941c098306be8b7efe32cd33c8670bb5be5
hencecrystalbright <hence.crystalbright@gmail.com>
2026-07-27 11:26:34 +0700

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ HEADERS = {"User-Agent": "Mozilla/5.0"}
 
 # --- Telegram 推播函數 ---
 def send_telegram_alert(message):
-    bot_token = "885051159:AAfYgXc9GaX6Mhjry4y_57tfKXA13t5IiU" 
+    bot_token = "8850511159:AAFygXc9GaX6Mhjry4y_57tfKXA13t5IilU"
     chat_id = "5259644398"
     url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
     payload = {
